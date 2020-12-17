87c2c7a528dc1e39bd5521c4b1af99be993a74d1
x00145570 <x00145570@outlook.com>
2020-12-17 14:03:02 +0000

    EAD Project #1

--- obj/Debug/netstandard2.1/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
+++ obj/Debug/netstandard2.1/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
@@ -13,63 +13,63 @@ namespace CA3_BlazorApp.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Nitesh Naik\source\repos\CA3_BlazorApp\_Imports.razor"
+#line 1 "N:\College\EAD\EAD_BLAZOR\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Nitesh Naik\source\repos\CA3_BlazorApp\_Imports.razor"
+#line 2 "N:\College\EAD\EAD_BLAZOR\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Nitesh Naik\source\repos\CA3_BlazorApp\_Imports.razor"
+#line 3 "N:\College\EAD\EAD_BLAZOR\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Nitesh Naik\source\repos\CA3_BlazorApp\_Imports.razor"
+#line 4 "N:\College\EAD\EAD_BLAZOR\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Nitesh Naik\source\repos\CA3_BlazorApp\_Imports.razor"
+#line 5 "N:\College\EAD\EAD_BLAZOR\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Nitesh Naik\source\repos\CA3_BlazorApp\_Imports.razor"
+#line 6 "N:\College\EAD\EAD_BLAZOR\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Nitesh Naik\source\repos\CA3_BlazorApp\_Imports.razor"
+#line 7 "N:\College\EAD\EAD_BLAZOR\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Nitesh Naik\source\repos\CA3_BlazorApp\_Imports.razor"
+#line 8 "N:\College\EAD\EAD_BLAZOR\_Imports.razor"
 using CA3_BlazorApp;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Nitesh Naik\source\repos\CA3_BlazorApp\_Imports.razor"
+#line 9 "N:\College\EAD\EAD_BLAZOR\_Imports.razor"
 using CA3_BlazorApp.Shared;
 
 #line default
@@ -83,7 +83,7 @@ using CA3_BlazorApp.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 12 "C:\Users\Nitesh Naik\source\repos\CA3_BlazorApp\Shared\SurveyPrompt.razor"
+#line 12 "N:\College\EAD\EAD_BLAZOR\Shared\SurveyPrompt.razor"
        
     // Demonstrates how a parent component can supply parameters
     [Parameter]
